e92c8858316723b8df1b6c01cd8697bf71437611
ritikashinde25 <143322128+ritikashinde25@users.noreply.github.com>
2026-04-06 10:11:02 +0530

    Add files via upload

---
+++ HibernateORMExp1/src/main/java/com/test/StoreData.java
@@ -0,0 +1,46 @@
+package com.test;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class StoreData {
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		
+		cfg.configure("hibernate.cfg.xml");  
+		
+		SessionFactory sf = cfg.buildSessionFactory();
+		
+		Session session = sf.openSession();
+		
+		Transaction t = session.beginTransaction();
+		
+		Employee emp = new Employee();
+		
+		emp.setEmp_name("Rohit");
+		emp.setEmp_company("BCC");
+		emp.setEmp_name("Mumbai");
+		
+		
+		Employee obj = session.find(Employee.class, 2);
+		
+		System.out.println(obj.getId()+" "+obj.getEmp_name()+" "+obj.getEmp_company()+" "+obj.getEmp_city());
+		obj.setEmp_name("Sharma");
+		obj.setEmp_company("PWC");
+		obj.setEmp_city("Banglore");
+		
+		
+		//session.persist(obj);
+		
+		session.remove(obj);
+		t.commit();
+		
+		
+		System.out.println("Done.");
+		
+		session.close(); 
+		
+	}
+}
