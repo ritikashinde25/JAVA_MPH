453e85573ec32304a5b696ed4619abdfb5f6d19c
ritikashinde25 <143322128+ritikashinde25@users.noreply.github.com>
2026-04-06 10:06:53 +0530

    Add files via upload

---
+++ HibernateInheritance/src/main/java/com/test/Vehicle.java
@@ -0,0 +1,53 @@
+package com.test;
+
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "vehicle")
+@Inheritance(strategy = InheritanceType.JOINED)
+
+//@DiscriminatorColumn( name = "vehicle_type", discriminatorType = DiscriminatorType.STRING )
+public class Vehicle {
+	private int id;
+	
+	private String name;
+	
+	private int licenseNumber;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getLicenseNumber() {
+		return licenseNumber;
+	}
+
+	public void setLicenseNumber(int licenseNumber) {
+		this.licenseNumber = licenseNumber;
+	}
+
+	
+	
+}
