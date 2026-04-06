e37c754212c63f17a19a61cce4c1a083c49e1eda
ritikashinde25 <143322128+ritikashinde25@users.noreply.github.com>
2026-04-06 10:49:04 +0530

    Add files via upload

---
+++ SpringAOP/src/main/java/com/test/spring/aspect/EmployeeAspectJoinPoint.java
@@ -0,0 +1,26 @@
+package com.test.spring.aspect;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class EmployeeAspectJoinPoint {
+	
+	@Before("execution(public void com.test.model..set*(*))")
+	public void loggingAdvice(JoinPoint joinpoint)
+	{
+		System.out.println("before executing loggin advice on method : "+joinpoint.toString());
+		
+		System.out.println("arguments paased : "+Arrays.toString(joinpoint.getArgs()));
+	}
+	
+	@Before("args(name)")
+	public void logStringArguments(String name)
+	{
+		System.out.println("String argument passed : "+name);
+	}
+	
+}
