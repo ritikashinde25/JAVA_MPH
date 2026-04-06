ef36581bfebcf82aff61864e6bca89cc157feddc
ritikashinde25 <143322128+ritikashinde25@users.noreply.github.com>
2026-04-06 10:52:01 +0530

    Add files via upload

---
+++ SpringBootRestJPAMySQL/src/main/java/com/test/dao/ProductDao.java
@@ -0,0 +1,28 @@
+package com.test.dao;
+
+import java.util.List;
+
+import com.test.entity.Product;
+
+public interface ProductDao {
+
+	Product saveProduct(Product obj);
+
+	Product getProductById(int id);
+
+	Product updateProductById(Product obj);
+
+	List<Product> deleteProductById(int id);
+
+	List<Product> getAllProducts();
+
+	List<Product> getProductsByName(String name);
+
+	List<Product> getProductByQuantity(int qunty);
+
+	List<Product> getAllProductsByNameASC();
+
+	List<Product> getProductsByNameQP(String name);
+
+	List<Product> getProductsByQuantityQP(int qnty);
+}
---
+++ SpringBootRestJPAMySQL/src/main/java/com/test/exceptions/ProductNotFoundException.java
@@ -0,0 +1,9 @@
+package com.test.exceptions;
+
+public class ProductNotFoundException extends RuntimeException
+{
+	public ProductNotFoundException(String msg)
+	{
+		super(msg);
+	}
+}
