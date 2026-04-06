5da2699c1eda9f195cf4e32a0b21e399e0cdb7b1
ritikashinde25 <143322128+ritikashinde25@users.noreply.github.com>
2026-04-06 11:33:21 +0530

    Add files via upload

---
+++ SpringBootMVCExp1/src/main/java/com/test/controller/BookController.java
@@ -0,0 +1,51 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.test.model.Book;
+
+@Controller
+public class BookController {
+	
+	@GetMapping("/")
+	public String homepage()
+	{
+		return "index";
+	}
+	
+	@GetMapping("/bform")
+	public String bookForm()
+	{
+		return "bookpage";
+	}
+	
+	@PostMapping("/submit")
+	public String submitBookForm(
+			@RequestParam("id") int id, 
+			@RequestParam("name") String name,
+			@RequestParam("author") String author,
+			@RequestParam("price") int price,
+			Model model, Book book)
+	{
+		book.setId(id);
+		book.setName(name);
+		book.setAuthor(name);
+		book.setPrice(price);
+		
+		model.addAttribute("data",book);
+		
+		if(book.getId()>0)
+		{
+			return "success";
+		}
+		else
+		{
+			return "bookpage";
+		}
+	}
+
+}
---
+++ SpringBootMVCExp1/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2><%= "Hello World! Spring MVC" %></h2>
+<a href = "bform">click here to add book</a>
+</body>
+</html>
